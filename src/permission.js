2b16a7dc6d92f0c1e29ee27d079dd51a13fb346e
sunxu <1017294721@qq.com>
2018-12-16 21:35:11 +0800

    12412

--- src/permission.js
+++ src/permission.js
@@ -2,7 +2,8 @@ import router from './router'
 import store from './store'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css'// progress bar style
-import { getToken } from '@/utils/auth' // getToken from cookie
+import { getToken,removeToken } from '@/utils/auth' // getToken from cookie
+import { Message } from 'element-ui';
 
 NProgress.configure({ showSpinner: false })// NProgress Configuration
 
@@ -19,6 +20,11 @@ router.beforeEach((to, from, next) => {
       if (store.getters.addRouters.length === 0) { // 判断当前用户是否已拉取完user_info信息
         const roles = ['admin'] // note: roles must be a array! such as: ['editor','develop']
         store.dispatch('GenerateRoutes', { roles }).then(() => { // 根据roles权限生成可访问的路由表
+          // if(store.getters.addRouters)
+          console.log(store.getters.addRouters);
+          if(store.getters.addRouters.length === 0){
+            removeToken();
+          }
           router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
           next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
         })
@@ -31,6 +37,7 @@ router.beforeEach((to, from, next) => {
     if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
       next()
     } else {
+      Message.warning("用户登陆已失效请重新登陆")
       next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
       NProgress.done()
     }
